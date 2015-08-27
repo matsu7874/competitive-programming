e422ff692a7fcab7fef4b625302c3d118a2a3cea
matsu7874 <mtsmtkmt@gmail.com>
2015-08-27 15:43:27 +0900

    modify

--- atcoder/abc002/c.py
+++ atcoder/abc002/c.py
@@ -1,3 +1,3 @@
 (xa, ya, xb, yb, xc, yc) = map(int, input().split())
-S = 0.5 * (abs((xa-xc)*(yb-yc) - (xb-xc)*(ya-yc)))
+S = 0.5 * (abs((xa - xc) * (yb - yc) - (xb - xc) * (ya - yc)))
 print(S)
--- atcoder/abc002/d.py
+++ atcoder/abc002/d.py
@@ -9,7 +9,7 @@ if M == 0:
     exit()
 
 for _ in range(M):
-    (x, y) = [int(x)-1 for x in input().split()]
+    (x, y) = [int(x) - 1 for x in input().split()]
     friend[x][y] = 1
     friend[y][x] = 1
 for i in range(N):
@@ -17,13 +17,13 @@ for i in range(N):
 for k in range(N):
     for i in range(N):
         for j in range(N):
-            friend[x][y] = min(friend[x][y], friend[x][k]+friend[k][y])
+            friend[x][y] = min(friend[x][y], friend[x][k] + friend[k][y])
 largest_clique = 1
 for i in range(2**N):
     clique = []
     are_everyone_friend = True
     for j in range(N):
-        if (i & (1<<j)) != 0:
+        if (i & (1 << j)) != 0:
             clique.append(j)
     for pair in itertools.combinations(clique, 2):
         if friend[pair[0]][pair[1]] >= NOT_FRIEND:
