000d88c3ad7bd36f8c2d58ee1fc94f05ddc4fc1e
matsu7874 <mtsmtkmt@gmail.com>
2015-08-28 16:15:07 +0900

    clear

--- atcoder/abc011/d.py
+++ atcoder/abc011/d.py
@@ -7,15 +7,3 @@ if X % D != 0 or Y % D != 0:
 X //= D
 Y //= D
 T = [[0 for j in range(2 * N + 1)] for i in range(2 * N + 1)]
-Direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-T[N][N] = 1
-for i in range(N):
-    Tn = [T[i][:] for i in range(N)]
-    for j in range(N):
-        for k in range(N):
-            if Tn[j][k] > 0:
-                for l in range(4):
-                    + Direction[l]
-    Tn[0][0] += 1
-    T = [Tn[i][:] for i in range(N)]
-    print(Tn[0])
