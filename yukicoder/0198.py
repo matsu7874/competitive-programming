cfc3b2b7eb1881dc48198f756259171e3d377ebf
matsu7874 <mtsmtkmt@gmail.com>
2015-10-20 01:27:12 +0900

    add comment to 198

--- yukicoder/0198.py
+++ yukicoder/0198.py
@@ -9,11 +9,11 @@ if N == 1:
     exit()
 
 l = 0
-r = (sum_c + B)//N + 1
+r = (sum_c + B) // N + 1
 total_l = 0
 total_r = 0
 
-for i in range(int(math.log(sum_c + B, 3)*200) + 2):
+for i in range(52): # 52 = ceil((8*log10+log11)/(log3-log2))
     ml = (l * 2 + r) // 3
     mr = (l + r * 2) // 3
     total_l = 0
@@ -25,5 +25,4 @@ for i in range(int(math.log(sum_c + B, 3)*200) + 2):
         r = mr
     else:
         l = ml
-    # print(l, ml, total_l, mr, total_r, r)
 print(min(total_l, total_r))
