d39d470e00925e21fba210323910aec904a77ffe
matsu7874 <mtsmtkmt@gmail.com>
2015-11-19 17:24:58 +0900

    solve 297

--- yukicoder/0297.py
+++ yukicoder/0297.py
@@ -8,4 +8,35 @@ for c in C:
         op.append(c)
     else:
         num.append(int(c))
-print(op,num)
+
+l = len(op)
+
+nums = []
+for i in range(l):
+    nums.append(num[i])
+t = 0
+for i in range(len(num)-len(op)):
+    t += num[len(op)+i]*10**i
+nums.append(t)
+nums.sort()
+max_v = nums[-1]
+for i in range(l):
+    if op[i] == '+':
+        max_v += nums[l-i-1]
+    else:
+        max_v -= nums[l-i-1]
+
+if '-' in op:
+    min_v = nums[0]
+    for i in range(l):
+        if op[i] == '+':
+            min_v += nums[i+1]
+        else:
+            min_v -= nums[i+1]
+else:
+    nums = [0]*(l+1)
+    for i in range(len(num)):
+        nums[i%(l+1)] *= 10
+        nums[i%(l+1)] += num[i]
+    min_v = sum(nums)
+print(max_v,min_v)
