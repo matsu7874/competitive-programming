587419b478391a1d99e19f22b54ef90f31032d0d
matsu7874 <mtsmtkmt@gmail.com>
2015-12-30 21:44:36 +0900

    write auto tester

--- yukicoder/test.py
+++ yukicoder/test.py
@@ -1,33 +1,55 @@
 import time
-import random
+import subprocess
+import sys
+import os
 
-A = [str(random.randint(0, 10000)) for i in range(50)]
-S = ' '.join(A)
-times = 10
+argvs = sys.argv
+current_directory = os.getcwd()
 
-print('method|elapsed[s]|type(A[0])')
-print('---|---|---')
-time_start_map = time.time()
-for i in range(times):
-    A = list(map(int, S.split()))
-time_elapsed_map = time.time() - time_start_map
-print('list(map)|', time_elapsed_map,'|',type(A[0]))
+path = (current_directory + '/' +
+        str(argvs[1]) + '-testcase/').replace('/', '\\')
+testcases = os.listdir(path + '\\test_in\\')
+if not os.path.exists(path + '\\my_out\\'):
+    os.mkdir(path + '\\my_out\\')
 
-time_start_map = time.time()
-for i in range(times):
-    A = [map(int, S.split())]
-time_elapsed_map = time.time() - time_start_map
-print('[map]|', time_elapsed_map,'|',type(A[0]))
+count_testcase = len(testcases)
+count_AC = 0
+count_WA = 0
+count_TLE = 0
 
+for case in testcases:
+    input_file = path + '\\test_in\\' + case
+    expected_file = input_file.replace('test_in', 'test_out')
+    output_file = input_file.replace('test_in', 'my_out')
 
-time_start_for = time.time()
-for i in range(times):
-    A = list(int(x) for x in S.split())
-time_elapsed_for = time.time() - time_start_for
-print('list(generator)|', time_elapsed_for,'|',type(A[0]))
+    cmd = ['python', './' + argvs[1] + '.py', '<', input_file, '>', output_file]
 
-time_start_for = time.time()
-for i in range(times):
-    A = [int(x) for x in S.split()]
-time_elapsed_for = time.time() - time_start_for
-print('[]|', time_elapsed_for,'|',type(A[0]))
+    start_time = time.time()
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        cp = p.communicate(timeout=1)
+        stdout_data = cp.stdout.decode('utf-8')
+        stderr_data = cp.stderr.decode('utf-8')
+    except subprocess.TimeoutExpired:
+        p.kill()
+        count_TLE += 1
+        status = 'TLE'
+    except Exception as e:
+        raise
+    else:
+        cmd = ['diff', '-q', expected_file, output_file]
+        cp = subprocess.run(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if 'differ' in cp.stdout.decode('utf-8'):
+            status = 'WA'
+            count_WA += 1
+        else:
+            status = 'AC'
+            count_AC += 1
+    elapsed_time = time.time() - start_time
+    print(status, str(round(elapsed_time, 7)) + '[s]', end=' ')
+    print(input_file.replace(path + '\\test_in\\', ''))
+
+print('AC', count_AC, '/', count_testcase)
+print('WA', count_WA, '/', count_testcase)
+print('TLE', count_TLE, '/', count_testcase)
