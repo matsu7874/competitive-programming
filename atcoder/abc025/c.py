ea4e4b3fdf8eb58a5b9d93667c220aa0c613169a
matsu7874 <mtsmtkmt@gmail.com>
2016-04-22 00:41:35 +0900

    speedup

--- atcoder/abc025/c.py
+++ atcoder/abc025/c.py
@@ -1,13 +1,15 @@
+BOARD_SIZE = 9
+
 b = [list(map(int, input().split())) for i in range(2)]
 c = [list(map(int, input().split())) for i in range(3)]
 
 sum_score = 0
-for lst in [b,c]:
-    for row in lst:
-        for e in row:
-            sum_score += e
+for i in range(3):
+    for j in range(2):
+        sum_score += b[j][i]
+        sum_score += c[i][j]
 
-BOARD_SIZE = 9
+
 def calc_score():
     score = 0
     for i in range(2):
@@ -18,15 +20,17 @@ def calc_score():
                 score += c[j][i]
     return score
 
+
 def dfs(cur):
-    res = float('inf') * (-1)**(cur+1)
     key = 0
     t = 1
     for i in range(BOARD_SIZE):
         key += board[i] * t
         t *= 3
-    if memo[cur % 2][key] > -1:
-        return memo[cur % 2][key]
+    if memo[key] > -1:
+        return memo[key]
+
+    res = float('inf') * (-1)**(cur+1)
     if cur == 9:
         res = calc_score()
     elif cur % 2 == 0:
@@ -41,10 +45,10 @@ def dfs(cur):
                 board[i] = 2
                 res = min(res, dfs(cur+1))
                 board[i] = 0
-    memo[cur % 2][key] = res
+    memo[key] = res
     return res
 
-memo = [[-1] * (3**9) for i in range(2)]
-board = [0 for i in range(BOARD_SIZE)]
+memo = [-1] * (3**9)
+board = [0]*9
 score = dfs(0)
 print(score, sum_score-score, sep='\n')
