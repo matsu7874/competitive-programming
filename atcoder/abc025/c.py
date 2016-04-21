eec91d34e94c792ce16d60c1a4d331f710156a4d
matsu7874 <mtsmtkmt@gmail.com>
2016-04-22 00:24:31 +0900

    solve abc025 c

--- atcoder/abc025/c.py
+++ atcoder/abc025/c.py
@@ -1,49 +1,50 @@
 b = [list(map(int, input().split())) for i in range(2)]
 c = [list(map(int, input().split())) for i in range(3)]
+
 sum_score = 0
 for lst in [b,c]:
     for row in lst:
         for e in row:
             sum_score += e
-def count_score(board):
-    key = ''.join(board)
-    if key in states:
-        return states[key]
+
+BOARD_SIZE = 9
+def calc_score():
     score = 0
     for i in range(2):
         for j in range(3):
             if board[i*3+j] == board[i*3+j+3]:
                 score += b[i][j]
-            else:
-                score -= b[i][j]
-    for i in range(3):
-        for j in range(2):
-            if board[i*3+j] == board[i*3+j+1]:
-                score += c[i][j]
-            else:
-                score -= c[i][j]
-    states[key] = score
+            if board[j*3+i] == board[j*3+i+1]:
+                score += c[j][i]
     return score
 
-def search(turn, board):
-    if turn == 9:
-        return count_score(board)
-    best_score = float('inf') * (-1)**(turn+1)
-    best_hand = 0
-    for i in range(9):
-        if board[i] == '':
-            next_board = board[:i] + ['ox'[turn%2]] + board[i+1:]
-            n_score = search(turn + 1, next_board)
-            if turn % 2 == 0:
-                if best_score < n_score:
-                    best_hand = i
-                    best_score = n_score
-            else:
-                if best_score > n_score:
-                    best_hand = i
-                    best_score = n_score
-    return best_score
+def dfs(cur):
+    res = float('inf') * (-1)**(cur+1)
+    key = 0
+    t = 1
+    for i in range(BOARD_SIZE):
+        key += board[i] * t
+        t *= 3
+    if memo[cur % 2][key] > -1:
+        return memo[cur % 2][key]
+    if cur == 9:
+        res = calc_score()
+    elif cur % 2 == 0:
+        for i in range(BOARD_SIZE):
+            if board[i] == 0:
+                board[i] = 1
+                res = max(res, dfs(cur+1))
+                board[i] = 0
+    else:
+        for i in range(BOARD_SIZE):
+            if board[i] == 0:
+                board[i] = 2
+                res = min(res, dfs(cur+1))
+                board[i] = 0
+    memo[cur % 2][key] = res
+    return res
 
-states = {}
-best_score = search(0,['','','','','','','','',''])
-print((sum_score+best_score)//2, (sum_score-best_score)//2)
+memo = [[-1] * (3**9) for i in range(2)]
+board = [0 for i in range(BOARD_SIZE)]
+score = dfs(0)
+print(score, sum_score-score, sep='\n')
