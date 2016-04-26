69c35bc87a5bac263b11a7f21607e8b44f1ecd57
matsu7874 <mtsmtkmt@gmail.com>
2016-04-27 00:26:37 +0900

    update to a*

--- atcoder/atc002/a.py
+++ atcoder/atc002/a.py
@@ -5,9 +5,9 @@ gy, gx = map(int, input().split())
 maze = [input() for i in range(r)]
 
 visited = [False for i in range(r*c)]
-hq = [(0, sy-1, sx-1)]
+hq = [(abs(gy-sy)**2 + abs(gx-sx), 0, sy-1, sx-1)]
 while hq:
-    cost, y, x = heapq.heappop(hq)
+    heuristic, cost, y, x = heapq.heappop(hq)
     if y == gy-1 and x == gx-1:
         print(cost)
         exit()
@@ -15,6 +15,7 @@ while hq:
     for dy,dx in [(1,0),(-1,0),(0,1),(0,-1)]:
         ny = y + dy
         nx = x + dx
+        heuristic = abs(gy - ny) + abs(gx - nx) + cost
         if maze[ny][nx] == '.' and not visited[ny*c+nx]:
-            heapq.heappush(hq,(cost+1, ny, nx))
+            heapq.heappush(hq,(heuristic,cost+1, ny, nx))
             visited[ny*c+nx] = True
