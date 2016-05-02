2e4ff525389bc6e85254fce21091d251e5c27a24
matsu7874 <mtsmtkmt@gmail.com>
2016-05-02 22:26:38 +0900

    solve arc032 b.js

--- atcoder/arc032/b.js
+++ atcoder/arc032/b.js
@@ -1,11 +1,16 @@
 function UnionFind(size){
   this._values = new Array(size);
-  this._values.fill(-1);
+  for(var i=0; i<size; ++i){
+      this._values[i] = -1;
+  }
 }
 
 UnionFind.prototype = {
   find: function(x){
     while(this._values[x] >= 0){
+        if(this._values[this._values[x]] >= 0){
+            this._values[x] = this._values[this._values[x]];
+        }
       x = this._values[x];
     }
     return x;
@@ -24,7 +29,7 @@ UnionFind.prototype = {
     return this._values[px];
     }
   },
-  count: function(){
+  countGroup: function(){
     var group = 0;
     for(var i = 0; i<this._values.length; ++i){
       if(this._values[i] < 0){
@@ -39,16 +44,18 @@ UnionFind.prototype = {
 function main(input){
   var rows = input.split("\n");
   var n,m;
-  n = parseInt(rows[0].split(" ")[0],10);
-  m = parseInt(rows[0].split(" ")[1],10);
+  var tmp = rows[0].split(" ");
+  n = parseInt(tmp[0],10);
+  m = parseInt(tmp[1],10);
   var uf = new UnionFind(n);
   for(var i=0; i<m; i++){
     var a,b;
-    a = parseInt(rows[i+1].split(" ")[0], 10) - 1;
-    b = parseInt(rows[i+1].split(" ")[1], 10) - 1;
+    tmp = rows[i+1].split(" ");
+    a = parseInt(tmp[0], 10) - 1;
+    b = parseInt(tmp[1], 10) - 1;
     uf.union(a,b);
   }
-  console.log(uf.count()-1);
+  console.log(uf.countGroup()-1);
 }
 
 main(require("fs").readFileSync("/dev/stdin", "utf8"));
