b83dd31b747ed71be51420823b1516e70625757a
matsu7874 <mtsmtkmt@gmail.com>
2016-05-22 02:09:51 +0900

    update matrix library

--- library/py/matrix.py
+++ library/py/matrix.py
@@ -1,3 +1,140 @@
+class Matrix():
+
+    def __init__(self, array):
+        self.r = len(array)
+        self.c = len(array[0])
+        self.matrix = []
+        for i in range(self.r):
+            self.matrix.append(array[i][:])
+
+    def component(self, i, j):
+        return self.matrix[i][j]
+
+    def row(self, n):
+        return self.matrix[n]
+
+    def column(self, n):
+        return [self.matrix[i][n] for i in range(self.r)]
+
+    def transpose(self):
+        return Matrix(list(map(list, zip(*self.matrix))))
+
+    def display(self):
+        for i in range(self.r):
+            print(self.matrix[i])
+
+    def copy(self):
+        return Matrix([[self.matrix[i][j] for j in range(self.c)] for i in range(self.r)])
+
+    def add(self, a):
+        b = self.copy()
+        for i in range(self.r):
+            for j in range(self.c):
+                b.matrix[i][j] += a.matrix[i][j]
+        return b
+
+    def subtract(self, a):
+        b = self.copy()
+        for i in range(self.r):
+            for j in range(self.c):
+                b.matrix[i][j] -= a.matrix[i][j]
+        return b
+
+    def times(self, real):
+        a = self.copy()
+        for i in range(self.r):
+            for j in range(self.c):
+                a.matrix[i][j] *= real
+        return a
+
+    def dot_product(self, a):
+        b = [[] for i in range(self.r)]
+        for i in range(self.r):
+            for j in range(a.c):
+                t = 0
+                for k in range(self.c):
+                    t += self.matrix[i][k] * a.matrix[k][j]
+                b[i].append(t)
+        return Matrix(b)
+
+    def rank(self, a):
+        # O(n m^2)
+        n = len(a)
+        m = len(a[0])
+
+        return 0
+
+    def determinant_naive(self):
+        # require regular matrix
+        if self.r == 1:
+            return self.matrix[0][0]
+        elif self.r == 2:
+            return self.matrix[0][0] * self.matrix[1][1] - self.matrix[1][0] * self.matrix[0][1]
+        det = 0
+        for i in range(n):
+            if self.matrix[i][0] == 0:
+                continue
+            cofactor = []
+            for j in range(0, n):
+                if i == j:
+                    continue
+                cofactor.append(self.matrix[j][1:])
+            sign = -1 if (i + j) % 2 else 1
+            cofactor = Matrix(cofactor)
+            det += sign * self.matrix[i][0] * cofactor.determinant_naive()
+        return det
+
+    def LU_decomposition(self):
+        n = self.r
+        L = Matrix([[0 for j in range(n)] for i in range(n)])
+        U = Matrix([[0 for j in range(n)] for i in range(n)])
+        for i in range(n):
+            L.matrix[i][i] = 1
+        for k in range(n):
+            U.matrix[k][k] = self.matrix[k][k]
+            for i in range(k+1, n):
+                L.matrix[i][k] = self.matrix[i][k] / U.matrix[k][k]
+                U.matrix[k][i] = self.matrix[k][i]
+            for i in range(k+1, n):
+                for j in range(k+1,n):
+                    self.matrix[i][j] -= L.matrix[i][k] * U.matrix[k][j]
+        return L,U
+
+
+    def gauss_jordan_elimination(self):
+        a = self.copy()
+        pivot = 0
+        L = min(self.r, self.c)
+        while pivot < L:
+            pivot_v = a.matrix[pivot][pivot]
+            pivot_row = pivot
+            for i in range(pivot + 1, L):
+                v = max(a.matrix[i][pivot], -a.matrix[i][pivot])
+                if pivot_v < v:
+                    pivot_row = i
+                    pivot_v = v
+            if pivot_row > pivot:
+                for i in range(self.c):
+                    a.matrix[pivot][i], a.matrix[pivot_row][i] = a.matrix[pivot_row][i], a.matrix[pivot][i]
+            if pivot_v == 0:
+                pivot += 1
+                continue
+            inv_pivot = 1 / a.matrix[pivot][pivot]
+            a.matrix[pivot][pivot] = 1
+            for i in range(pivot+1, self.c):
+                a.matrix[pivot][i] *= inv_pivot
+            for i in range(self.r):
+                if i == pivot:
+                    continue
+                t = -1 * a.matrix[i][pivot]
+                a.matrix[i][pivot] = 0
+                for j in range(pivot+1,self.c):
+                    a.matrix[i][j] += t * a.matrix[pivot][j]
+            pivot += 1
+
+        return (a)
+
+
 def matrix2_product(a, b):
     p1 = (a[0][0] + a[1][1]) * (b[0][0] + b[1][1])
     p2 = (a[1][0] + a[1][1]) * b[0][0]
@@ -29,5 +166,26 @@ def matrix2_power(mtr, p):
     return ans
 
 if __name__ == '__main__':
-    a,b = matrix2_power([[1,1],[1,0]],int(input()))
-    print(sum(a))
+    n = 3
+    A = [[i * 100 + j for j in range(n)] for i in range(n)]
+    B = [[i * 100  for j in range(n)] for i in range(n)]
+    I = [[2 if i == j else 0 for j in range(n)] for i in range(n)]
+    A = [[2,3,1,5],[6,13,5,19],[2,19,10,23],[4,10,11,31]]
+    a = Matrix(A)
+    b = Matrix(B)
+    a.display()
+    b.display()
+
+    # a.dot_product(b).display()
+    print(a.determinant_naive())
+
+    # import time
+    # START_TIME = time.time()
+    # at = a.transpose()
+    # ELAPSED_TIME = time.time() - START_TIME
+    # print('elapsed time is:', ELAPSED_TIME, '[s]')
+    l,u = a.LU_decomposition()
+    l.display()
+    u.display()
+    a.gauss_jordan_elimination().display()
+    b.gauss_jordan_elimination().display()
