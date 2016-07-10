44d7dc23c436b910c9edb8a954440a1014467293
unknown <mtsmtkmt@gmail.com>
2016-07-11 00:04:27 +0900

    solve 304

--- yukicoder/0304.py
+++ yukicoder/0304.py
@@ -1,4 +1,8 @@
-import sys
-for i in range(1000, 2000):
-    print(str(i)[1:])
-    sys.stdout.flush()
+import itertools
+
+res = ''
+for code in itertools.product([str(i) for i in range(10)], repeat=3):
+    print(''.join(code), flush=True)
+    res = input()
+    if res == 'unlocked':
+        exit()
