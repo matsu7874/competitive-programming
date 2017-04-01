3040e3bdcd552b4d4cdd151351758e00da3ab3d2
unknown <mtsmtkmt@gmail.com>
2017-04-01 21:47:04 +0900

    b200

--- atcoder/agc012/b.py
+++ atcoder/agc012/b.py
@@ -14,26 +14,25 @@ for i in range(Q):
     v,d,c = map(int, input().split())
     shots.append((v-1, d, c))
 colors = [0 for i in range(N)]
-sizes = [0 for i in range(N)]
+sizes = [-1 for i in range(N)]
 edges = [[i] for i in range(N)]
 for i in range(Q-1, -1, -1):
     v, d, c = shots[i]
-    if colors[v] == 0:
-        colors[v] = c
-        dq = collections.deque()
-
-        for nv in nodes[v]:
-            dq.append((1, nv))
-        visited = [False for i in range(N)]
-        visited[v] = True
-        while dq:
-            dist, idx = dq.popleft()
-            visited[idx] = True
-            if colors[idx] == 0:
-                colors[idx] = c
-            if dist >= d:
-                continue
-            for nv in nodes[idx]:
-                if not visited[nv]:
-                    dq.append((dist+1, nv))
+    dq = collections.deque()
+    dq.append((d, v))
+    visited = [False for i in range(N)]
+    while dq:
+        size, idx = dq.popleft()
+        if sizes[idx] >= size:
+            continue
+        else:
+            sizes[idx] = size
+        visited[idx] = True
+        if colors[idx] == 0:
+            colors[idx] = c
+        if size >= d:
+            continue
+        for nv in nodes[idx]:
+            if not visited[nv]:
+                dq.append((size+1, nv))
 print(*colors, sep='\n')
